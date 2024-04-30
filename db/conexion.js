3f1b09e600f5030d3f7f98b36cb6596823dde008
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-04-29 20:10:50 -0600

    Commit dot env

--- db/conexion.js
+++ db/conexion.js
@@ -1,7 +1,16 @@
 import pg from 'pg-promise'
 const pgp = pg();
 
-const cnstr = `postgresql://postgres:Hola12@localhost:5432/postgres`;
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+const user_db= process.env.user_db;
+const user_pass= process.env.user_pass;
+const host= process.env.host;
+const database= process.env.database;
+
+const cnstr = `postgresql://${user_db}:${user_pass}@${host}:5432/${database}`;
 
 const db = pgp(cnstr);
 
